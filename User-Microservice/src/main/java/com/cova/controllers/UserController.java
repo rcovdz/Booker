72fe1d4bbf10c331c30283e92d2e3a8b94e34dac
rcovdev <rcovdev@gmail.com>
2025-08-07 20:55:01 -0600

    User Validation and Lombok

--- User-Microservice/src/main/java/com/cova/controllers/UserController.java
+++ User-Microservice/src/main/java/com/cova/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.cova.controllers;
 
 import com.cova.modals.User;
 import com.cova.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("api/users")
-    public User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody @Valid User user) {
         return userRepository.save(user);
     }
 
@@ -43,6 +44,7 @@ public class UserController {
             existingUser.setLastName(user.getLastName());
             existingUser.setUsername(user.getUsername());
             existingUser.setEmail(user.getEmail());
+            existingUser.setPassword(user.getPassword());
             existingUser.setPhone(user.getPhone());
             existingUser.setRole(user.getRole());
 
--- User-Microservice/src/main/java/com/cova/modals/User.java
+++ User-Microservice/src/main/java/com/cova/modals/User.java
@@ -4,119 +4,58 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "First name is required...")
+    @Size(max = 30, message = "First name must not exceed 30 characters...")
     private String firstName;
+
+    @NotBlank(message = "Last name is required...")
+    @Size(max = 30, message = "Last name must not exceed 30 characters...")
     private String lastName;
+
+    @NotBlank(message = "Username is required...")
+    @Size(min = 5, max = 30, message = "Username must be between 5 and 30 characters...")
     private String username;
+
+    @NotBlank(message = "Email is required...")
+    @Email(message = "Please enter a valid email address...")
     private String email;
+
+    @NotBlank(message = "Password is required...")
+    @Size(min = 10, max = 30, message = "Password must be between 10 and 30 characters...")
+    private String password;
+
+    @Size(min = 10, max = 10, message = "Phone number must have 10 digits...")
     private String phone;
+
+    @NotBlank(message = "Role name is required...")
     private String role;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    // No-args constructor
-    public User() {};
-
-    // Constructor
-    public User(Long id,
-                String firstName,
-                String lastName,
-                String username,
-                String email,
-                String phone,
-                String role,
-                LocalDateTime createdAt,
-                LocalDateTime updatedAt) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.email = email;
-        this.phone = phone;
-        this.role = role;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    // Setter Methods
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    // Getter Methods
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
 }
